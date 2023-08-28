24454e2fc096ddc492d24079ac19d0f75d9e19e9
Tanzhor_RIK <tanzhor4@gmail.com>
2023-08-29 02:10:16 +0700

    homework 27.1

---
+++ course/views.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets
+
+from course.models import Course
+from .serializers import CourseSerializer
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
