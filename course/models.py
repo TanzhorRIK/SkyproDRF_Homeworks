f61f3d234f1cc3de936cd1dbfd9d61c7f9950440
Tanzhor_RIK <tanzhor4@gmail.com>
2023-09-04 01:18:48 +0700

    add relationbs

--- course/models.py
+++ course/models.py
@@ -6,6 +6,7 @@ class Course(models.Model):
     name = models.CharField(max_length=100, verbose_name="название")
     image = models.ImageField(verbose_name='изображение', **NULLABLE)
     description = models.TextField(verbose_name="описание")
+    owner = models.ForeignKey('users.User', on_delete=models.CASCADE, verbose_name='Владелец', **NULLABLE)
 
     def __str__(self):
         return f'{self.name}'
@@ -16,12 +17,14 @@ class Course(models.Model):
 
 
 class Lesson(models.Model):
+    course = models.ForeignKey('Course', on_delete=models.CASCADE, **NULLABLE)
     name = models.CharField(max_length=100, verbose_name="название")
     description = models.TextField(verbose_name="описание")
     image = models.ImageField(upload_to='lesson/', verbose_name='изображение',
                               **NULLABLE)
     video_url = models.CharField(max_length=255, verbose_name="ссылка на видео",
                                  **NULLABLE)
+    owner = models.ForeignKey('users.User', on_delete=models.CASCADE, verbose_name='Владелец', **NULLABLE)
 
 
     def __str__(self):
@@ -30,3 +33,22 @@ class Lesson(models.Model):
     class Meta:
         verbose_name = 'урок'
         verbose_name_plural = 'уроки'
+
+
+
+class Payments(models.Model):
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='payments', verbose_name='пользователь')
+    date_pay = models.DateField(auto_now_add=True, verbose_name='дата оплаты')
+    type_metod = models.CharField(choices=(('Course', 'Курс'), ('Lesson', 'Урок')), verbose_name='оплаченный курс или урок')
+    sum_pay = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='сумма оплаты')
+    payment_method = models.CharField(choices=(('Cash', 'Наличные '), ('Account_transfer', 'Перевод на счет')), verbose_name='наличные или перевод на счет')
+
+
+
+    def __str__(self):
+        return f'{self.user}, {self.date_pay}, {self.sum_pay}'
+
+    class Meta:
+        verbose_name = 'платёж'
+        verbose_name_plural = 'платежи'
+
